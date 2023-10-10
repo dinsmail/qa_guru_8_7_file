8029e0c9d786b00c6ec5eb3e3c7b4631b7e41dd0
Dinara_Kokhanovskaya <d.kohanovskaya@ipoteka.center>
2023-10-10 23:46:17 +0300

    content check

--- test_zipfile.py
+++ test_zipfile.py
@@ -1,5 +1,7 @@
 import os
 from zipfile import ZipFile
+import pandas as pd
+from PyPDF2 import PdfReader
 from openpyxl import load_workbook
 
 zipFilesDir = './resources'
@@ -41,6 +43,44 @@ def test_files_are_same():
             zip_size = zip_item.file_size
             file_size = os.path.getsize(filename)
             assert zip_size == file_size
+
+
+def test_txt():
+    create_zip()
+    with ZipFile(zipPath, 'r') as testZip:
+        assert 'АлтайскийКрай\n' in testZip.read('regions.txt').decode('utf-8')
+
+
+def test_pdf():
+    create_zip()
+    with ZipFile(zipPath) as testZip:
+        with testZip.open("sprafka.pdf") as pdf_f:
+            pdf_file = PdfReader(pdf_f)
+            number_of_page = len(pdf_file.pages)
+            assert number_of_page == 1
+            assert 'Справка о доходах для получения кредита' in pdf_file.pages[0].extract_text()
+
+
+def test_xls():
+    create_zip()
+    with ZipFile(zipPath) as testZip:
+        with testZip.open("file_example.xls") as xls_f:
+            text = pd.read_excel(xls_f).head(2)
+            assert 'Hashimoto' in text['Last Name'].values
+
+
+def test_xlsx():
+    create_zip()
+    with ZipFile(zipPath) as testZip:
+        with testZip.open("file_123.xlsx") as xlsx_f:
+            xlsx_file = load_workbook(xlsx_f)
+            sheet = xlsx_file.active
+            sheet_value = sheet.cell(row=14, column=3).value
+            num_rows = sheet.max_row
+            num_cols = sheet.max_column
+            assert sheet_value == 'Ascencio'
+            assert num_rows == 51
+            assert num_cols == 8
 
 
 def test_check_zip_is_removed():
